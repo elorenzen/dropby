2e9feba061a00c3e4fe1d0343f65c53f721405a9
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-08-12 18:12:42 -0700

    Create folders, files from following Nuxt.js3 docs

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+    <NuxtPage />
   </div>
 </template>
+
+<script>
+/*
+Here, we're going to need to do a few things.
+
+First, check if cookies are stored.
+
+If so, we need to write a 'getUserRole()' function that get's
+user's set type from Db, then will render pages based on role.
+
+else, show schedule/map component
+
+Ie.
+"'user logged in as 'Merchant'" --> Landing page at '/' will render different view component than if role == 'Vendor'
+If so, get user role.
+
+*/
+</script>
