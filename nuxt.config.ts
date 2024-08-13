7027ae796dd49ad1c8e3d12c98273fb7de4f30ac
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-08-12 18:54:44 -0700

    Error handling by setting Supabase configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase"]
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false // set to 'true', EVENTUALLY
+  }
 })
