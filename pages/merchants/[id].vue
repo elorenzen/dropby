538aa9cc8c80990f16f43bd7e624daae15701d7c
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-09-04 11:17:08 -0700

    Add function to get selected merchant data from route param

--- pages/merchants/[id].vue
+++ pages/merchants/[id].vue
@@ -1,10 +1,27 @@
 <template>
     <span>Merchant ID: {{ $route.params.id }}</span>
+    {{ merchant }}
   </template>
   
   <script setup>
   const route = useRoute()
+  const supabase = useSupabaseClient()
   console.log(route.params) // { id: '123' }
+  const merchant = ref('')
+
+  async function getMerchant() {
+    const { data, error } = await supabase
+        .from('merchants')
+        .select()
+        .eq('id', route.params.id)
+    console.log(data)
+    console.log(error)
+    merchant.value = data
+  }
+
+  onMounted(() => {
+    getMerchant()
+  })
   </script>
 
 <style lang="scss" scoped>
