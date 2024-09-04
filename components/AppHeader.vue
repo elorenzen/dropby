9d67ad514df43ac8ba3d09a43b29aee3ad3b4434
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-09-04 13:22:57 -0700

    Refactor to login/register with email, password credentials

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,21 +1,77 @@
 <script setup lang="ts">
-const route = useRoute()
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+console.log(user.value)
 
-const links = [
-  [{
-    label: 'New Merchant',
-    icon: 'i-heroicons-user',
-    to: '/merchantSignup'
-  },
-  {
-    label: 'New Merchant User',
-    icon: 'i-heroicons-user',
-    to: '/newMerchantUser'
-  }]
-]
+const loading = ref(false)
+const email = ref('')
+const password = ref('')
+
+const fireAuth = async () => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  })
+  if (error) {
+    const { data, error } = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value,
+    })
+  }
+
+}
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  email.value = ''
+  password.value = ''
+}
+
 </script>
 
 <template>
-  <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+    <v-toolbar color="orange-darken-4">
+      <v-toolbar-title>DropBy</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-if="!user"
+        density="compact"
+        variant="solo-filled"
+        class="mt-5 mx-1"
+        v-model="email"
+        placeholder="Email address"
+      ></v-text-field>
+
+      <v-text-field
+      v-if="!user"
+        density="compact"
+        variant="solo-filled"
+        class="mt-5 mx-1"
+        v-model="password"
+        type="password"
+        placeholder="Password"
+      ></v-text-field>
+
+      <v-btn
+        v-if="!user"
+        @click="fireAuth"
+        :disabled="email == '' || password == ''"
+        class="mx-1"
+        color="black"
+        variant="outlined">
+        Login/Register
+      </v-btn>
+
+      <span v-if="user">{{ user.email }}</span>
+      <v-btn
+        v-if="user"
+        @click="signOut"
+        class="mx-1"
+        color="black"
+        variant="outlined">
+        Sign Out
+      </v-btn>
+    </v-toolbar>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
         <div class="flex justify-center p-2">
             <h1 class="text-4xl">Welcome to DropBy!</h1>
         </div>
-        <Account v-if="user" />
-        <Auth v-else />
+        <!-- <Account v-if="user" />
+        <Auth v-else /> -->
         <MerchantList />
     </div>
     </div>
