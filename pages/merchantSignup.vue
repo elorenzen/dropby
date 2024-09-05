d5d06fbe545f0b0f84c1ff3c6e92b260c8d24aea
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-09-04 18:05:59 -0700

    Styling

--- pages/merchantSignup.vue
+++ pages/merchantSignup.vue
@@ -6,7 +6,5 @@ const user = useSupabaseUser()
   <div class="flex justify-center p-2">
       <h2 class="text-4xl">New Merchant</h2>
   </div>
-  <div class="container">
-    <NewMerchant />
-  </div>
+  <NewMerchant />
 </template>
