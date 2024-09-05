0112e8fa677fefdb75a530ec8fa1a0b573709719
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-09-04 17:22:49 -0700

    Styling

--- pages/signup.vue
+++ pages/signup.vue
@@ -3,23 +3,25 @@ const user = useSupabaseUser()
 </script>
 
 <template>
-  <div class="flex justify-center p-2">
-      <h1 class="text-4xl">Sign Up as...</h1>
-      <v-row>
-    <v-btn
-        class="mx-1"
-        color="black"
-        variant="outlined">
-        Merchant
-      </v-btn>
-  </v-row>
-  <v-row>
-    <v-btn
-        class="mx-1"
-        color="black"
-        variant="outlined">
-        Vendor
-      </v-btn>
-  </v-row>
-  </div>
+    <v-row class="flex justify-center p-2">
+        <h2 class="text-4xl">Sign Up as...</h2>
+    </v-row>
+    <v-row class="flex justify-center p-2">
+        <v-btn
+            class="mx-1"
+            color="orange-darken-4"
+            variant="outlined">
+            <NuxtLink to="/merchantSignup">
+                Merchant
+            </NuxtLink>
+        </v-btn>
+        <v-btn
+            class="mx-1"
+            color="orange-darken-4"
+            variant="outlined">
+            <NuxtLink to="/vendorSignup">
+                Vendor
+            </NuxtLink>
+        </v-btn>
+    </v-row>
 </template>
