b349ac1ad7cc53467a5e55bca24169ce9df7c7d6
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-09-12 13:46:59 -0700

    working changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <script setup>
+/**
+ * COLOR PALETTE
+ * Orange(primary): #e28413
+ * Dark(secondary): #000022
+ * White(accent): #fbf5f3
+ */
+
+const user = useSupabaseUser()
+
+// const { data } = await supabase
+//   .from('profiles')
+//   .select(`username, website, avatar_url`)
+//   .eq('id', user.value.id)
+//   .single()
 </script>
 
 <template>
     <div>
       <div>
         <div class="flex justify-center p-2">
-            <h1 class="text-4xl">Welcome to DropBy!</h1>
+            <h3 class="text-2xl">Welcome to DropBy!</h3>
         </div>
         <!-- <Account v-if="user" />
         <Auth v-else /> -->
-        <MerchantList />
+        <MerchantList v-if="user" />
         <MapView />
     </div>
     </div>
