d67e1ac63234843a41eae80d1b683d1281c4585c
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-10-11 09:33:24 -0700

    Change conditions for UI components, file cleanup

--- components/merchant/MerchantCard.vue
+++ components/merchant/MerchantCard.vue
@@ -1,51 +1,5 @@
 <template>
-    <Card v-if="!isAdmin" style="overflow: hidden">
-        <template #header>
-            <img alt="user header" :src="merchant.avatar_url" />
-        </template>
-        <template #title>{{ merchant.merchant_name }}</template>
-        <template #subtitle>{{ merchant.merchant_description }}</template>
-        <template #content>
-          <v-row>
-            <v-btn prepend-icon="mdi-map-marker" variant="plain" class="mt-2" readonly>
-              <template v-slot:prepend><v-icon></v-icon></template>
-              <NuxtLink>{{ merchant.formatted_address ? merchant.formatted_address : 'No address on file' }}</NuxtLink>
-            </v-btn>
-          </v-row>
-          <v-row>
-            <v-btn prepend-icon="mdi-phone" variant="plain" readonly>
-              <template v-slot:prepend><v-icon></v-icon></template>
-              <NuxtLink>{{ merchant.phone }}</NuxtLink>
-            </v-btn>
-          </v-row>
-          <v-row>
-            <v-btn prepend-icon="mdi-web" variant="plain">
-              <template v-slot:prepend><v-icon></v-icon></template>
-              <NuxtLink :to="merchant.website" target="_blank">Website</NuxtLink>
-            </v-btn>
-          </v-row>
-          <v-row>
-            <v-btn prepend-icon="mdi-instagram" variant="plain">
-              <template v-slot:prepend><v-icon></v-icon></template>
-              <NuxtLink :to="merchant.instagram" target="_blank">Instagram</NuxtLink>
-            </v-btn>
-          </v-row>
-          <v-row>
-            <v-btn prepend-icon="mdi-email" variant="plain">
-              <template v-slot:prepend><v-icon></v-icon></template>
-              <NuxtLink :to="`mailto:${merchant.email}`" target="_blank">Email</NuxtLink>
-            </v-btn>
-          </v-row>
-        </template>
-        <!-- <template #footer>
-            <div class="flex gap-4 mt-1">
-                <Button label="Cancel" severity="secondary" outlined class="w-full" />
-                <Button label="Save" class="w-full" />
-            </div>
-        </template> -->
-    </Card>
-
-    <Card v-else style="overflow: hidden">
+    <Card v-if="storeUser && storeUser.is_admin && storeUser.type == 'merchant'" style="overflow: hidden">
         <template #header>
             <img alt="user header" :src="merchant.avatar_url" />
         </template>
@@ -108,71 +62,59 @@
             </div>
         </template> -->
     </Card>
+
+    <Card v-else style="overflow: hidden">
+        <template #header>
+            <img alt="user header" :src="merchant.avatar_url" />
+        </template>
+        <template #title>{{ merchant.merchant_name }}</template>
+        <template #subtitle>{{ merchant.merchant_description }}</template>
+        <template #content>
+          <v-row>
+            <v-btn prepend-icon="mdi-map-marker" variant="plain" class="mt-2" readonly>
+              <template v-slot:prepend><v-icon></v-icon></template>
+              <NuxtLink>{{ merchant.formatted_address ? merchant.formatted_address : 'No address on file' }}</NuxtLink>
+            </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn prepend-icon="mdi-phone" variant="plain" readonly>
+              <template v-slot:prepend><v-icon></v-icon></template>
+              <NuxtLink>{{ merchant.phone }}</NuxtLink>
+            </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn prepend-icon="mdi-web" variant="plain">
+              <template v-slot:prepend><v-icon></v-icon></template>
+              <NuxtLink :to="merchant.website" target="_blank">Website</NuxtLink>
+            </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn prepend-icon="mdi-instagram" variant="plain">
+              <template v-slot:prepend><v-icon></v-icon></template>
+              <NuxtLink :to="merchant.instagram" target="_blank">Instagram</NuxtLink>
+            </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn prepend-icon="mdi-email" variant="plain">
+              <template v-slot:prepend><v-icon></v-icon></template>
+              <NuxtLink :to="`mailto:${merchant.email}`" target="_blank">Email</NuxtLink>
+            </v-btn>
+          </v-row>
+        </template>
+        <!-- <template #footer>
+            <div class="flex gap-4 mt-1">
+                <Button label="Cancel" severity="secondary" outlined class="w-full" />
+                <Button label="Save" class="w-full" />
+            </div>
+        </template> -->
+    </Card>
 </template>
 
 <script setup>
-const loading = ref(true)
 const props = defineProps(['merchant']);
 const merchant = ref(props.merchant)
-const supabase = useSupabaseClient()
-const sessionUser = useSupabaseUser()
-const isAdmin = ref(false)
-const avatar_path = ref('')
-
-onMounted(async () => {
-  if (sessionUser) {
-    const { data } = await supabase
-      .from('users')
-      .select()
-      .eq('id', sessionUser.value.id)
-    const foundUser = data[0]
-    console.log(foundUser)
-    if (
-      foundUser &&
-      foundUser.is_admin &&
-      foundUser.associated_merchant_id == merchant.value.id
-    ) isAdmin.value = true
-    console.log('admin value: ', isAdmin.value)
-  }
-  console.log('sessionUser: ', sessionUser.value)
-})
-
-const { data, error } = await supabase
-  .from('merchants')
-  .select(`id, avatar_url`)
-  .eq('id', merchant.value.id)
-  .single()
-
-if (data) {
-  avatar_path.value = data.avatar_url
-} else console.log(error)
-
-loading.value = false
-
-async function updateAvatar(e) {
-  if (e) {
-    try {
-      loading.value = true
-
-      const updates = {
-        avatar_url: e,
-        updated_at: new Date(),
-      }
-
-      const { error } = await supabase
-        .from('merchants')
-        .update(updates)
-        .eq('id', merchant.value.id)
-
-      if (error) throw error
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      loading.value = false
-    }
-  }
-}
-loading.value = false
+const store = useUserStore()
+const storeUser = store.user
 </script>
 
 <style lang="scss" scoped></style>
--- components/vendor/VendorCard.vue
+++ components/vendor/VendorCard.vue
@@ -1,51 +1,5 @@
 <template>
-  <Card v-if="!isAdmin" style="overflow: hidden">
-      <template #header>
-          <img alt="user header" :src="vendor.avatar_url" />
-      </template>
-      <template #title>{{ vendor.vendor_name }}</template>
-      <template #subtitle>{{ vendor.vendor_description }}</template>
-      <template #content>
-        <v-row>
-          <v-btn prepend-icon="mdi-map-marker" variant="plain" class="mt-2" readonly>
-            <template v-slot:prepend><v-icon></v-icon></template>
-            <NuxtLink>{{ vendor.formatted_address ? vendor.formatted_address : 'No address on file' }}</NuxtLink>
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn prepend-icon="mdi-phone" variant="plain" readonly>
-            <template v-slot:prepend><v-icon></v-icon></template>
-            <NuxtLink>{{ vendor.phone }}</NuxtLink>
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn prepend-icon="mdi-web" variant="plain">
-            <template v-slot:prepend><v-icon></v-icon></template>
-            <NuxtLink :to="vendor.website" target="_blank">Website</NuxtLink>
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn prepend-icon="mdi-instagram" variant="plain">
-            <template v-slot:prepend><v-icon></v-icon></template>
-            <NuxtLink :to="vendor.instagram" target="_blank">Instagram</NuxtLink>
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn prepend-icon="mdi-email" variant="plain">
-            <template v-slot:prepend><v-icon></v-icon></template>
-            <NuxtLink :to="`mailto:${vendor.email}`" target="_blank">Email</NuxtLink>
-          </v-btn>
-        </v-row>
-      </template>
-      <!-- <template #footer>
-          <div class="flex gap-4 mt-1">
-              <Button label="Cancel" severity="secondary" outlined class="w-full" />
-              <Button label="Save" class="w-full" />
-          </div>
-      </template> -->
-  </Card>
-
-  <Card v-else style="overflow: hidden">
+  <Card v-if="storeUser && storeUser.is_admin && storeUser.type == 'vendor'" style="overflow: hidden">
       <template #header>
           <img alt="user header" :src="vendor.avatar_url" />
       </template>
@@ -108,43 +62,58 @@
           </div>
       </template> -->
   </Card>
+  <Card v-else style="overflow: hidden">
+      <template #header>
+          <img alt="user header" :src="vendor.avatar_url" />
+      </template>
+      <template #title>{{ vendor.vendor_name }}</template>
+      <template #subtitle>{{ vendor.vendor_description }}</template>
+      <template #content>
+        <v-row>
+          <v-btn prepend-icon="mdi-map-marker" variant="plain" class="mt-2" readonly>
+            <template v-slot:prepend><v-icon></v-icon></template>
+            <NuxtLink>{{ vendor.formatted_address ? vendor.formatted_address : 'No address on file' }}</NuxtLink>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn prepend-icon="mdi-phone" variant="plain" readonly>
+            <template v-slot:prepend><v-icon></v-icon></template>
+            <NuxtLink>{{ vendor.phone }}</NuxtLink>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn prepend-icon="mdi-web" variant="plain">
+            <template v-slot:prepend><v-icon></v-icon></template>
+            <NuxtLink :to="vendor.website" target="_blank">Website</NuxtLink>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn prepend-icon="mdi-instagram" variant="plain">
+            <template v-slot:prepend><v-icon></v-icon></template>
+            <NuxtLink :to="vendor.instagram" target="_blank">Instagram</NuxtLink>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn prepend-icon="mdi-email" variant="plain">
+            <template v-slot:prepend><v-icon></v-icon></template>
+            <NuxtLink :to="`mailto:${vendor.email}`" target="_blank">Email</NuxtLink>
+          </v-btn>
+        </v-row>
+      </template>
+      <!-- <template #footer>
+          <div class="flex gap-4 mt-1">
+              <Button label="Cancel" severity="secondary" outlined class="w-full" />
+              <Button label="Save" class="w-full" />
+          </div>
+      </template> -->
+  </Card>
 </template>
 
 <script setup>
-const loading = ref(true)
 const props = defineProps(['vendor']);
 const vendor = ref(props.vendor)
-const supabase = useSupabaseClient()
-const sessionUser = useSupabaseUser()
-const isAdmin = ref(false)
-const avatar_path = ref('')
-
-onMounted(async () => {
-  if (sessionUser) {
-    const { data } = await supabase
-      .from('users')
-      .select()
-      .eq('id', sessionUser.value.id)
-    const foundUser = data[0]
-    console.log(foundUser)
-    if (
-      foundUser &&
-      foundUser.is_admin &&
-      foundUser.associated_vendor_id == vendor.value.id
-    ) isAdmin.value = true
-  }
-})
-
-const { data, error } = await supabase
-  .from('vendors')
-  .select(`id, avatar_url`)
-  .eq('id', vendor.value.id)
-  .single()
-
-if (data) avatar_path.value = data.avatar_url
-else console.log(error)
-
-loading.value = false
+const store = useUserStore()
+const storeUser = store.user
 </script>
 
 <style lang="scss" scoped></style>
