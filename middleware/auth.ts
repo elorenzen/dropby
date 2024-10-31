fe22935e47f63502790aa84c9dabf32935dc1530
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-10-31 14:51:46 -0700

    Refactor auth middleware to consider user roles(merchants, vendors)

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,23 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const supabase = useSupabaseClient()
     const user = useSupabaseUser()
     const userStore = useUserStore()
-    console.log('to: ', to)
-    console.log('from: ', from)
-    console.log('user: ', userStore.getUser)
+
     if (user.value) {
+        const { data } = await supabase
+            .from('users')
+            .select()
+            .eq('id', user.value.id)
+        await userStore.fetchUser(data && data.length > 0 ? data[0] : '')
+    }
+
+    const storeUser = userStore.getUser
+
+    if (
+        user.value &&
+        storeUser &&
+        (to.params.id == storeUser[`associated_${storeUser.type}_id`])
+    ) {
         return
     }
     return navigateTo('/')
