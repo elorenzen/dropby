675e9b8dba1fdab39ae4906353376c5689a8a6a4
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-11-07 20:08:35 -0800

    Add basic page, tab panels for eventual component files

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-        ADMIN SHIT HERE!
-    </div>
+    <div class="card">
+    <Tabs v-model:value="value">
+        <TabList>
+            <Tab value="0">Analytics</Tab>
+            <Tab value="1">Users</Tab>
+            <Tab value="2">Establishments</Tab>
+            <Tab value="3">Food Trucks</Tab>
+            <Tab value="4">Support Tickets</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel value="0">Charts and shit here</TabPanel>
+            <TabPanel value="1">
+                <h4>Table of associated admin users</h4>
+                <p><strong>to-do</strong></p>
+                <p>add 'switch' fields to set platform feature access based on new user roles(dev/customer support/etc.)</p>
+            </TabPanel>
+            <TabPanel value="2">
+                <p><strong>to-do</strong></p>
+                <p>table column additions: billing info, activity, usage metrics</p>
+                <p>table actions: 'ban'/hide merchants/vendors, activate 'promoted' status</p>
+                <MerchantList />
+            </TabPanel>
+            <TabPanel value="3">
+                <p><strong>to-do</strong></p>
+                <p>table column additions: billing info, activity, usage metrics</p>
+                <p>table actions: 'ban'/hide merchants/vendors, activate 'promoted' status</p>
+                <VendorList />
+            </TabPanel>
+            <TabPanel value="4">Table of user-submitted support tickets</TabPanel>
+        </TabPanels>
+    </Tabs>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
     middleware: ['auth']
 })
+const value = ref('0');
 </script>
 
 <style scoped>
