55593a36151a28569b8a31fb29174a3d8f964874
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-11-13 14:16:16 -0800

    Add basic 'view event' dialog shown on calendar dayclick event

--- components/EventCalendar.vue
+++ components/EventCalendar.vue
@@ -1,51 +1,71 @@
 <template>
   <div>
     <VCalendar is-dark expanded :attributes="attributes" @dayclick="openDayView" />
-    <Dialog v-model:visible="dayViewDialog" modal :header="dayId" :style="{ width: '25rem' }">
-      {{ dayDate }}    
+    <Dialog v-model:visible="dayViewDialog" modal :header="new Date(dayDate).toLocaleDateString()" :style="{ width: '35rem' }">
+      <Card v-if="eventOnDay" style="width: 30rem; overflow: hidden">
+          <template #title>
+            {{ new Date(eventOnDay.start).toLocaleTimeString('en-US') }} - {{ new Date(eventOnDay.end).toLocaleTimeString('en-US') }}
+            <span>
+              <Tag :value="eventOnDay.status" :severity="getStatusLabel(eventOnDay.status)" />
+            </span>
+          </template>
+          <template #subtitle>
+            <NuxtLink :to="eventOnDay.location_url" target="_blank">{{ eventOnDay.location_address }}</NuxtLink>
+          </template>
+          <template #content>
+              <p class="m-0">{{ eventOnDay.notes }}</p>
+          </template>
+          <template #footer>
+              <div class="flex gap-4 mt-1">
+                  <Button label="Edit" severity="secondary" outlined class="w-full" />
+                  <Button label="Delete" severity="danger" class="w-full" />
+              </div>
+          </template>
+      </Card>
+      <Card v-else style="width: 30rem; overflow: hidden">
+        <template #content>
+          No event scheduled. Create one now...
+        </template>
+      </Card>
+      <div v-else>No events scheduled. Add one now...</div>
     </Dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps(['id'])
-  const idParam = ref(props.id)
-  const eventStore = useEventStore()
+  const props         = defineProps(['id'])
+  const idParam       = ref(props.id)
+
+  const eventStore    = useEventStore()
   const merchantStore = useMerchantStore()
-  const merchant = ref(await merchantStore.getMerchantById(idParam.value))
-  const events = ref(await eventStore.getEventsByMerchantId(idParam.value))
+
+  const merchant      = ref(await merchantStore.getMerchantById(idParam.value))
+  const events        = ref(await eventStore.getEventsByMerchantId(idParam.value))
   const businessHours = ref(JSON.parse(JSON.stringify((merchant.value.business_hours))))
   businessHours.value = businessHours.value.map((day: any) => JSON.parse(day))
 
-  const date = ref()
+  const date          = ref()
+  const dayViewDialog = ref(false)
+  const dayId         = ref()
+  const dayDate       = ref()
+  const eventOnDay    = ref()
+
+  watch(date, (newVal) => {
+      if (!newVal) console.log('new date: ', newVal)
+  })
 
   const allOpenDates = computed(() => {
-    const allOpenEvents = events.value.filter(e => e.status === 'open')
-    return allOpenEvents.map(e => new Date(e.start))
+    const allOpenEvents = events.value.filter((e: any) => e.status === 'open')
+    return allOpenEvents.map((e: any) => new Date(e.start))
   })
   const allBookedDates = computed(() => {
-    const allBookedEvents = events.value.filter(e => e.status === 'booked')
-    return allBookedEvents.map(e => new Date(e.start))
+    const allBookedEvents = events.value.filter((e: any) => e.status === 'booked')
+    return allBookedEvents.map((e: any) => new Date(e.start))
   })
   const allPendingDates = computed(() => {
-    const allPendingEvents = events.value.filter(e => e.status === 'pending')
-    return allPendingEvents.map(e => new Date(e.start))
+    const allPendingEvents = events.value.filter((e: any) => e.status === 'pending')
+    return allPendingEvents.map((e: any) => new Date(e.start))
   })
-  
-  // const days = ref([])
-  // const selectMultipleDays = (day: any) => {
-  //   const idx = days.value.findIndex((d: any) => d.id === day.id);
-  //   if (idx >= 0) {
-  //     days.value.splice(idx, 1);
-  //   } else {
-  //     days.value.push({
-  //       id: day.id,
-  //       date: day.date,
-  //     });
-  //   }
-  // }
-
-  const dayViewDialog = ref(false)
 
   const attributes = ref([
     {
@@ -54,7 +74,7 @@
     },
     {
       highlight: {
-          color: 'gray',
+          color: 'blue',
           fillMode: 'light',
         },
       dates: allOpenDates.value
@@ -74,18 +94,48 @@
       dates: allBookedDates.value
     }
   ]);
-  watch(date, (newVal) => {
-      if (!newVal) console.log('new date: ', newVal)
-  })
-  const dayId = ref()
-  const dayDate = ref()
+
+  
   const openDayView = (day: any) => {
-    dayViewDialog.value = true
     dayId.value = day.id
     dayDate.value = day.date
-    console.log('day.id: ', day.id)
-    console.log('day.date: ', day.date)
+    eventOnDay.value = events.value
+      .find((e: any) => new Date(e.start).toDateString() == new Date(day.date).toDateString())
+    dayViewDialog.value = true
   }
+  const getStatusLabel = (status: any) => {
+      switch (status) {
+          case 'open':
+              return 'secondary';
+
+          case 'pending':
+              return 'warn';
+
+          case 'booked':
+              return 'success';
+
+          case 'unfulfilled':
+              return 'danger';
+          
+          case 'closed':
+              return 'secondary';
+
+          default:
+              return null;
+      }
+  };
+  // const days = ref([])
+  // const selectMultipleDays = (day: any) => {
+  //   const idx = days.value.findIndex((d: any) => d.id === day.id);
+  //   if (idx >= 0) {
+  //     days.value.splice(idx, 1);
+  //   } else {
+  //     days.value.push({
+  //       id: day.id,
+  //       date: day.date,
+  //     });
+  //   }
+  // }
 </script>
 
 <style scoped>
