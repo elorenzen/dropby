88a98bcf7bda9bd62625b64b681026d064702074
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-11-14 15:18:51 -0800

    Begin auth refactor

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,27 +1,82 @@
-<script setup>
-const user = useSupabaseUser()
-</script>
-
 <template>
-    <v-row class="flex justify-center p-2">
-        <h2 class="text-4xl">Sign Up as...</h2>
-    </v-row>
-    <v-row class="flex justify-center p-2">
+    <div class="flex justify-content-center flex-wrap">
+        <Card style="width: 30rem; overflow: hidden">
+            <template #title>
+                Sign Up
+            </template>
+            <template #content>
+                <Fluid>
+                    <div class="flex-auto">
+                        <FloatLabel variant="on" class="my-4">
+                            <InputText id="email" v-model="email" />
+                            <label for="email">Email</label>
+                        </FloatLabel>
+                    </div>
+                    <div class="flex-auto">
+                        <FloatLabel variant="on" class="my-4">
+                            <Password id="password" v-model="password" />
+                            <label for="password">Password</label>
+                        </FloatLabel>
+                    </div>
+                </Fluid>
+            </template>
+            <template #footer>
+                <Button
+                    @click="addAuthUser"
+                    label="Submit"
+                    severity="success"
+                    class="w-full"
+                    :disabled="!email || !password"
+                    :loading="loading"
+                ></Button>
+            </template>
+        </Card>
+    </div>
+    <!-- <div class="flex items-center gap-2">
+        <InputText placeholder="Email" v-model="email" type="text" class="w-32 sm:w-auto" />
+        <Password placeholder="Password" v-model="password" class="w-32 sm:w-auto" />
+    </div> -->
+    <v-snackbar
+      v-model="snackbar"
+      timeout="6000"
+    >
+      {{ snacktext }}
+
+      <template v-slot:actions>
         <v-btn
-            class="mx-1"
-            color="#e28413"
-            variant="outlined">
-            <NuxtLink to="/merchantSignup">
-                Merchant
-            </NuxtLink>
+          color="#000022"
+          variant="text"
+          @click="snackbar = false"
+        >
+          Close
         </v-btn>
-        <v-btn
-            class="mx-1"
-            color="#e28413"
-            variant="outlined">
-            <NuxtLink to="/vendorSignup">
-                Vendor
-            </NuxtLink>
-        </v-btn>
-    </v-row>
+      </template>
+    </v-snackbar>
 </template>
+
+<script setup lang="ts">
+const supabase  = useSupabaseClient()
+const email     = ref()
+const password  = ref()
+const loading   = ref(false)
+const snackbar  = ref(false)
+const snacktext = ref('')
+
+const addAuthUser = async () => {
+    const { error: authErr } = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value
+    })
+    if (!authErr) {
+        await useFetch(`/api/sendRegistrationConfirmation?email=${email.value}&password=${password.value}`)
+        snackbar.value = true
+        snacktext.value = "New user registered! Check your email for further instructions."
+
+        await navigateTo('/')
+    } else {
+        errType.value = 'User Registration'
+        errMsg.value = dbErr.message
+        errDialog.value = true
+    }
+}
+</script>
