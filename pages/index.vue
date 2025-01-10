bb759eebb00c4983d813328b8754e32a5d52fd36
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-01-09 17:46:55 -0800

    Begin development of homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
+        <div class="my-4 flex justify-around justify-center">
+            <div class="text-6xl" style="max-width: 50%; color: #e28413;">We make event scheduling easy!</div>
+        </div>
         <div class="flex justify-center p-2">
-            <h3 class="text-2xl">Welcome to DropBy!</h3>
+          <div style="max-width: 40%; color: #e28413; text-align: center;">
+            Good drinks belong with good eats,
+            and every food truck should have a spot to serve a hungry crowd.
+            With <span class="font-bold">DropBy</span>, businesses can find each other and book events,
+            increasing sales for both, and improving the overall experience for a shared customer base.
+          </div>
         </div>
-        <div>
-          <MerchantGallery />
+
+        <div class="flex items-center justify-center">
+          <MerchantGallery style="max-width: 75%;" />
         </div>
-        <div>
-          <VendorGallery />
+        <div class="flex items-center justify-center">
+          <VendorGallery style="max-width: 75%;" />
         </div>
     </div>
 </template>
