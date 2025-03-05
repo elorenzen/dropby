d07a69b1c65a45dc4265a4c4e61b0a2c02ad928e
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-03-05 12:20:24 -0800

    UI improvements

--- pages/viewer/map.vue
+++ pages/viewer/map.vue
@@ -1,22 +1,24 @@
 <template>
-    <!-- <div>
-        <MapView />
-    </div> -->
     <div class="ma-2">
-        <Tabs value="0">
-            <TabList>
-                <Tab value="0">Locations</Tab>
-                <Tab value="1">Food Trucks</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel value="0">
-                    <MerchantList />
-                </TabPanel>
-                <TabPanel value="1">
-                    <VendorList />
-                </TabPanel>
-            </TabPanels>
-        </Tabs>
+        <div>
+            <MapView />
+        </div>
+        <div>
+            <Tabs value="0">
+                <TabList>
+                    <Tab value="0">Locations</Tab>
+                    <Tab value="1">Food Trucks</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel value="0">
+                        <MerchantList />
+                    </TabPanel>
+                    <TabPanel value="1">
+                        <VendorList />
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </div>
     </div>
 </template>
 
