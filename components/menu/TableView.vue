49bf5fb0c0bc1bee4c54762b5006c633050d267d
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-03-05 03:30:07 -0800

    Vendor table UI improvements

---
+++ components/menu/TableView.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="ml-8">
+        <Card v-if="vendor.vendor_description">
+            <template #subtitle>Vendor Description</template>
+            <template #content>
+                <p class="m-0">{{ vendor.vendor_description }}</p>
+            </template>
+        </Card>
+        <DataTable :value="menuItems" class="ml-4" size="small">
+            <Column field="name" header="Menu Item" sortable></Column>
+            <Column header="Image">
+                <template #body="{ data }">
+                    <NuxtImg v-if="data.image_url" :src="data.image_url" :alt="data.name" class="w-24 h-16 rounded" />
+                </template>
+            </Column>
+            <Column field="price" header="Price" sortable>
+                <template #body="slotProps">
+                    {{ formatCurrency(slotProps.data.price) }}
+                </template>
+            </Column>
+            <Column field="description" header="Description" style="max-width: 20rem;">
+                <template #body="{ data }">
+                    <ScrollPanel style="height: 10vh;">
+                        <p class="m-0">{{ data.description }}</p>
+                    </ScrollPanel>
+                </template>
+            </Column>
+            <Column field="type" header="Type" sortable></Column>
+            <template #footer>
+                <span class="font-medium text-surface-500 dark:text-surface-400 text-xs" style="color: gray;">
+                    Total: {{ menuItems ? menuItems.length : 0 }}
+                </span>
+            </template>
+        </DataTable>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props        = defineProps(['vendorId'])
+const store        = useMenuStore()
+const vendorStore  = useVendorStore()
+const vendorId     = props.vendorId
+const vendor       = ref(await vendorStore.getVendorById(vendorId))
+const menuItems    = computed(() => store.menuItems.filter((i:any) => i.vendor_id === vendorId))
+const sortKey      = ref();
+const sortOrder    = ref();
+const sortField    = ref();
+const sortOptions  = ref([
+    {label: 'Price High to Low', value: '!price'},
+    {label: 'Price Low to High', value: 'price'},
+]);
+
+const onSortChange = (event:any) => {
+    const value = event.value.value;
+    const sortValue = event.value;
+
+    if (value.indexOf('!') === 0) {
+        sortOrder.value = -1;
+        sortField.value = value.substring(1, value.length);
+        sortKey.value = sortValue;
+    }
+    else {
+        sortOrder.value = 1;
+        sortField.value = value;
+        sortKey.value = sortValue;
+    }
+}
+const formatCurrency = (value:any) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+};
+</script>
+
+<style scoped>
+
+</style>
--- components/vendor/List.vue
+++ components/vendor/List.vue
@@ -1,49 +1,86 @@
 <template>
-  <DataTable
-      :value="vendors"
-      sortField="average_merchant_rating" :sort-order="-1"
-  >
-      <Column field="vendor_name" header="Name" sortable>
-          <template #body="slotProps">
-              {{ slotProps.data.vendor_name }}
-          </template>
-      </Column>
-      <Column field="cuisine" header="Cuisine">
-          <template #body="slotProps">
-            <Badge class="mx-1" v-for="(i, index) in slotProps.data.cuisine" :key="`${i}-${index}`">{{ i }}</Badge>
-          </template>
-      </Column>
-      <Column field="average_merchant_rating" header="Rating" :sortable="true">
-          <template #body="slotProps">
-            <Rating v-model="slotProps.data.average_merchant_rating" />
-          </template>
-      </Column>
-      <Column field="socials" header="">
-          <template #body="slotProps">
-            <!-- 
-            WILL BE READONLY WITH TOOLTIP
-            <Button icon variant="plain">
-                <NuxtLink :to="`tel:${slotProps.data.phone}`" target="_blank">
-                    <v-icon>mdi-phone</v-icon>
-                </NuxtLink>
-            </Button> -->
-            <Button icon="pi pi-globe" variant="plain">
-                <NuxtLink :to="slotProps.data.website" target="_blank" />
-            </Button>
-            <Button icon="pi pi-instagram" variant="plain">
-                <NuxtLink :to="slotProps.data.instagram" target="_blank" />
-            </Button>
-            <Button icon="pi pi-envelope" variant="plain">
-              <NuxtLink :to="`mailto:${slotProps.data.email}`" target="_blank" />
-            </Button>
-          </template>
-      </Column>
-  </DataTable>
-  </template>
+    <DataTable
+        :value="vendors"
+        sortField="average_merchant_rating"
+        :sort-order="-1"
+        :expandedRows="expandedRows"
+        dataKey="id"
+        @rowExpand="onRowExpand"
+        @rowCollapse="onRowCollapse"
+    >
+        <template #header>
+            <div class="flex flex-wrap justify-end gap-2">
+                <Button text icon="pi pi-plus" label="Expand All" @click="expandAll" />
+                <Button text icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
+            </div>
+        </template>
+        <Column expander style="width: 5rem" />
+        <Column field="vendor_name" header="Vendor" sortable>
+            <template #body="slotProps">
+                <p class="m-0 font-semibold">{{ slotProps.data.vendor_name }}</p>
+            </template>
+        </Column>
+        <Column field="cuisine" header="Cuisine">
+            <template #body="slotProps">
+                <Badge class="mx-1" v-for="(i, index) in slotProps.data.cuisine" :key="`${i}-${index}`">{{ i }}</Badge>
+            </template>
+        </Column>
+        <Column field="average_merchant_rating" header="Rating" :sortable="true">
+            <template #body="slotProps">
+                <Rating v-model="slotProps.data.average_merchant_rating" />
+            </template>
+        </Column>
+        <Column field="socials" header="">
+            <template #body="slotProps">
+                <Button
+                    as="a"
+                    size="small"
+                    class="mr-1"
+                    icon="pi pi-globe"
+                    variant="text"
+                    :href="slotProps.data.website"
+                    rounded
+                    target="_blank"
+                />
+                <Button
+                    as="a"
+                    size="small"
+                    class="mr-1"
+                    icon="pi pi-instagram"
+                    variant="text"
+                    :href="slotProps.data.instagram"
+                    rounded
+                    target="_blank"
+                />
+                <Button
+                    as="a"
+                    size="small"
+                    class="mr-1"
+                    icon="pi pi-envelope"
+                    variant="text"
+                    :href="`mailto:${slotProps.data.email}`"
+                    rounded
+                    target="_blank"
+                />
+            </template>
+        </Column>
+        <template #expansion="slotProps">
+            <MenuTableView :vendorId="slotProps.data.id" />
+        </template>
+    </DataTable>
+</template>
 
 <script setup>
 const vendorStore = useVendorStore()
 const vendors = vendorStore.getAllVendors
+const expandedRows = ref({})
+
+const expandAll = () => {
+    expandedRows.value = vendors.reduce((acc, p) => (acc[p.id] = true) && acc, {});
+};
+const collapseAll = () => {
+    expandedRows.value = null;
+};
 </script>
 
 <style lang="scss" scoped>
