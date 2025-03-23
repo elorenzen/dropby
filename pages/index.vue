263eb92e5f60d45369c3c5657a8e86757976e6a0
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-03-22 22:44:04 -0700

    Restyling

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="m-4 text-surface-700 dark:text-surface-100 text-center flex flex-col items-center gap-4">
-            <div class=" font-bold text-4xl leading-tight text-primary-500">Event Scheduling Made Easy!</div>
-            <div class=" text-xl leading-normal text-primary-500" style="width: 50%;">
+            <div class=" font-bold text-4xl leading-tight">Event Scheduling Made Easy!</div>
+            <div class=" text-xl leading-normal" style="width: 50%;">
               Good drinks belong with good eats,
               and every food truck should have a spot to serve a hungry crowd.
               With <span class="font-bold" style="color: #FF8906;">DropBy</span>, businesses can find each other and book events,
