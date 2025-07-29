165a3ef4b1ece3ae8e2f8b59cfc29dae53fd663e
Eric Lorenzen <eric.lorenzen@gmail.com>
2025-07-28 18:12:07 -0700

    Fix Stripe API versioning issues

--- server/api/payments/create-payment-intent.ts
+++ server/api/payments/create-payment-intent.ts
@@ -2,7 +2,7 @@ import { serverSupabaseClient } from '#supabase/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia'
+  apiVersion: '2025-06-30.basil'
 })
 
 export default defineEventHandler(async (event) => {
